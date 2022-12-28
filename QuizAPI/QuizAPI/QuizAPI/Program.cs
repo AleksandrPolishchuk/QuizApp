b2c04310ccbe37eb7134387b99b73f128e71d788
AP <aleksandrpolishchuk@gmail.com>
2022-12-28 22:49:03 +0700

    add MSSQL connect options

--- QuizAPI/QuizAPI/QuizAPI/Program.cs
+++ QuizAPI/QuizAPI/QuizAPI/Program.cs
@@ -1,3 +1,5 @@
+using QuizAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.ServicesAddDbContext<QuizDbContext>(options => options.UseSqlServer());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
