dce7bb39b149431c71f69bb32a909b1a30937a46
AP <aleksandrpolishchuk@gmail.com>
2023-01-10 22:42:49 +0700

    add components in login page

--- quiz-client/src/components/Login.js
+++ quiz-client/src/components/Login.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <form noValidate autoComplete="off">
+      <TextField label="Email" name="email" variant="outlined" />
+      <TextField label="Name" name="name" variant="outlined" />
+      <Button type="submit" variant="contained" size="large">
+        Start
+      </Button>
+    </form>
+  );
 }
