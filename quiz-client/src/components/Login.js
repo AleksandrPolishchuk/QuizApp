6f9fcf2b0e0beee515a03979afdbf3fc402e0512
AP <aleksandrpolishchuk@gmail.com>
2023-01-10 22:56:59 +0700

    add Box in login component

--- quiz-client/src/components/Login.js
+++ quiz-client/src/components/Login.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Button, TextField } from "@mui/material";
+import { Box } from "@mui/system";
 
 export default function Login() {
   return (
-    <form noValidate autoComplete="off">
-      <TextField label="Email" name="email" variant="outlined" />
-      <TextField label="Name" name="name" variant="outlined" />
-      <Button type="submit" variant="contained" size="large">
-        Start
-      </Button>
-    </form>
+    <Box
+      sx={{
+        "& .MuiTextField-root": {
+          m: 1,
+        },
+      }}
+    >
+      <form noValidate autoComplete="off">
+        <TextField label="Email" name="email" variant="outlined" />
+        <TextField label="Name" name="name" variant="outlined" />
+        <Button type="submit" variant="contained" size="large">
+          Start
+        </Button>
+      </form>
+    </Box>
   );
 }
