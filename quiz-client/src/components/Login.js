5bf5593da3dbbd1723b90f7fdb99ad3cb537412e
AP <aleksandrpolishchuk@gmail.com>
2023-01-10 23:14:36 +0700

    add center component

--- quiz-client/src/components/Login.js
+++ quiz-client/src/components/Login.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, Card, CardContent, TextField } from "@mui/material";
 import { Box } from "@mui/system";
+import Center from "./Center";
 
 export default function Login() {
   return (
-    <Box
-      sx={{
-        "& .MuiTextField-root": {
-          m: 1,
-        },
-      }}
-    >
-      <form noValidate autoComplete="off">
-        <TextField label="Email" name="email" variant="outlined" />
-        <TextField label="Name" name="name" variant="outlined" />
-        <Button type="submit" variant="contained" size="large">
-          Start
-        </Button>
-      </form>
-    </Box>
+    <Center>
+      <Card sx={{ width: 400 }}>
+        <CardContent>
+          <Box
+            sx={{
+              "& .MuiTextField-root": {
+                m: 1,
+                width: "90%",
+              },
+            }}
+          >
+            <form noValidate autoComplete="off">
+              <TextField label="Email" name="email" variant="outlined" />
+              <TextField label="Name" name="name" variant="outlined" />
+              <Button type="submit" variant="contained" size="large">
+                Start
+              </Button>
+            </form>
+          </Box>
+        </CardContent>
+      </Card>
+    </Center>
   );
 }
