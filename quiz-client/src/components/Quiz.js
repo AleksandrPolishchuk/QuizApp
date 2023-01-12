9f46e0301bac27a13bed4ea47c6f7647ac5ae890
AP <aleksandrpolishchuk@gmail.com>
2023-01-12 23:19:02 +0700

    add Quiz.js component

---
+++ quiz-client/src/components/Quiz.js
@@ -0,0 +1,8 @@
+import React from "react";
+import useStateContext from "../hooks/useStateContext";
+
+export default function Quiz() {
+  const { context, setContext } = useStateContext();
+
+  return <div>Question</div>;
+}
