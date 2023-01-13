02330b0a27544e9b686ce6aeb8174ccb89ba423c
AP <aleksandrpolishchuk@gmail.com>
2023-01-14 02:28:24 +0700

    add linear index in Quiz.js

--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -2,37 +2,68 @@ import {
   Card,
   CardContent,
   CardHeader,
+  LinearProgress,
   List,
   ListItemButton,
   Typography,
 } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { createAPIEndpoint, ENDPOINTS } from "../api";
+import { getFormatedTime } from "../helper";
 
 export default function Quiz() {
   const [qns, setQns] = useState([]);
   const [qnIndex, setQnIndex] = useState(0);
+  const [timeTaken, setTimeTaken] = useState(0);
+  let timer;
+
+  const startTimer = () => {
+    timer = setInterval(() => {
+      setTimeTaken((prev) => prev + 1);
+    }, [1000]);
+  };
 
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.question)
       .fetch()
       .then((res) => {
         setQns(res.data);
-        console.log(res.data);
+        startTimer();
       })
       .catch((err) => {
         console.log(err);
       });
+
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return qns.length !== 0 ? (
-    <Card sx={{ maxWidth: 640, mx: "auto", mt: 5 }}>
-      <CardHeader title={"Question " + (qnIndex + 1) + " of 5"} />
+    <Card
+      sx={{
+        maxWidth: 640,
+        mx: "auto",
+        mt: 5,
+        "& .MuiCardHeader-action": { m: 0, alignSelf: "center" },
+      }}
+    >
+      <CardHeader
+        title={"Question " + (qnIndex + 1) + " of 5"}
+        action={<Typography>{getFormatedTime(timeTaken)}</Typography>}
+      />
+      <Box>
+        <LinearProgress
+          variant="determinate"
+          value={((qnIndex + 1) * 100) / 5}
+        />
+      </Box>
       <CardContent>
         <Typography variant="h6">{qns[qnIndex].qnInwords}</Typography>
         <List>
           {qns[qnIndex].options.map((item, idx) => (
-            <ListItemButton key={idx} disableRipple>
+            <ListItemButton key={idx}>
               <div>
                 <b>{String.fromCharCode(65 + idx) + " . "}</b> {item}
               </div>
