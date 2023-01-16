bb0595120b8abd21d47e9fc183aecc2662812cf3
AP <aleksandrpolishchuk@gmail.com>
2023-01-16 23:57:30 +0700

    modify Result.js

--- quiz-client/src/api/index.js
+++ quiz-client/src/api/index.js
@@ -5,6 +5,7 @@ export const BASE_URL = "http://localhost:5031/";
 export const ENDPOINTS = {
   participant: "participant",
   question: "question",
+  getAnswers: "question/getanswers",
 };
 
 export const createAPIEndpoint = (endpoint) => {
--- quiz-client/src/components/Result.js
+++ quiz-client/src/components/Result.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { createAPIEndpoint, ENDPOINTS } from "../api";
+import useStateContext from "../hooks/useStateContext";
 
 export default function Result() {
+  const { context, setContext } = useStateContext();
+
+  useEffect(() => {
+    const ids = context.selectedOptions.map((x) => x.qnId);
+    createAPIEndpoint(ENDPOINTS.getAnswers)
+      .post(ids)
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
+
   return <div>Result</div>;
 }
