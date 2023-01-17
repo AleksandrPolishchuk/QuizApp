324dfc2cd611c260b6d65805286c653b40803e63
AP <aleksandrpolishchuk@gmail.com>
2023-01-17 22:35:03 +0700

    add Card with results in Result.js

--- quiz-client/src/components/Result.js
+++ quiz-client/src/components/Result.js
@@ -1,11 +1,14 @@
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { typography } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { createAPIEndpoint, ENDPOINTS } from "../api";
+import { getFormatedTime } from "../helper";
 import useStateContext from "../hooks/useStateContext";
 
 export default function Result() {
   const { context, setContext } = useStateContext();
-  const { score, setScore } = useState(0);
-  const { qnAnswers, setQnAnswers } = useState([]);
+  const [score, setScore] = useState(0);
+  const [qnAnswers, setQnAnswers] = useState([]);
 
   useEffect(() => {
     const ids = context.selectedOptions.map((x) => x.qnId);
@@ -28,5 +31,23 @@ export default function Result() {
     setScore(tempScore);
   };
 
-  return <div>Result</div>;
+  return (
+    <Card
+      sx={{ mt: 5, display: "flex", width: "100%", maxWidth: 640, mx: "auto" }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+        <CardContent sx={{ flex: "1 0 auto", textAlign: "center" }}>
+          <Typography variant="h4">Congradulations!</Typography>
+          <Typography variant="h6">YOUR SCORE</Typography>
+          <Typography variant="h5" sx={{ fontWeight: 600 }}>
+            <Typography variant="span">{score}</Typography>/5
+          </Typography>
+          <Typography variant="h6">
+            Took {getFormatedTime(context.timeTaken) + " mins"}
+          </Typography>
+        </CardContent>
+      </Box>
+      <CardMedia component="img" sx={{ width: 220 }} image="./result.png" />
+    </Card>
+  );
 }
