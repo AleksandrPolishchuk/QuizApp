2acd1096bd9bfb8164f797d662da1bb603227e41
AP <aleksandrpolishchuk@gmail.com>
2023-01-17 23:56:09 +0700

    add Answer.js

---
+++ quiz-client/src/components/Answer.js
@@ -0,0 +1,47 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  CardMedia,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { BASE_URL } from "../api";
+
+export default function Answer({ qnAnswers }) {
+  return (
+    <Box sx={{ mt: 5, width: "100%", maxWidth: 640, mx: "auto" }}>
+      {qnAnswers.map((item, j) => (
+        <Accordion disableGutters key={j}>
+          <AccordionSummary>
+            <Typography sx={{ width: "90%", flexShrink: 0 }}>
+              {item.qnInWords}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {item.imageName ? (
+              <CardMedia
+                component="img"
+                image={BASE_URL + "images/" + item.imageName}
+                sx={{ m: "10px auto", width: "auto" }}
+              />
+            ) : null}
+            <List>
+              {item.options.map((x, i) => (
+                <ListItem key={i}>
+                  <Typography>
+                    <b>{String.fromCharCode(65 + i) + " . "}</b>
+                    {x}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
+  );
+}
--- quiz-client/src/components/Result.js
+++ quiz-client/src/components/Result.js
@@ -7,13 +7,13 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import { typography } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createAPIEndpoint, ENDPOINTS } from "../api";
 import { getFormatedTime } from "../helper";
 import useStateContext from "../hooks/useStateContext";
 import { green } from "@mui/material/colors";
+import Answer from "./Answer";
 
 export default function Result() {
   const { context, setContext } = useStateContext();
@@ -70,52 +70,61 @@ export default function Result() {
   };
 
   return (
-    <Card
-      sx={{ mt: 5, display: "flex", width: "100%", maxWidth: 640, mx: "auto" }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-        <CardContent sx={{ flex: "1 0 auto", textAlign: "center" }}>
-          <Typography variant="h4">Congradulations!</Typography>
-          <Typography variant="h6">YOUR SCORE</Typography>
-          <Typography variant="h5" sx={{ fontWeight: 600 }}>
-            <Typography variant="span" color={green[500]}>
-              {score}
+    <>
+      <Card
+        sx={{
+          mt: 5,
+          display: "flex",
+          width: "100%",
+          maxWidth: 640,
+          mx: "auto",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+          <CardContent sx={{ flex: "1 0 auto", textAlign: "center" }}>
+            <Typography variant="h4">Congradulations!</Typography>
+            <Typography variant="h6">YOUR SCORE</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 600 }}>
+              <Typography variant="span" color={green[500]}>
+                {score}
+              </Typography>
+              /5
             </Typography>
-            /5
-          </Typography>
-          <Typography variant="h6">
-            Took {getFormatedTime(context.timeTaken) + " mins"}
-          </Typography>
-          <Button
-            variant="contained"
-            sx={{ mx: 1 }}
-            size="small"
-            onClick={submitScore}
-          >
-            Submit
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ mx: 1 }}
-            size="small"
-            onClick={restart}
-          >
-            Re-try
-          </Button>
-          <Alert
-            severity="success"
-            variant="string"
-            sx={{
-              width: "60%",
-              m: "auto",
-              visibility: showAlert ? "visible" : "hidden",
-            }}
-          >
-            Score Updated.
-          </Alert>
-        </CardContent>
-      </Box>
-      <CardMedia component="img" sx={{ width: 220 }} image="./result.png" />
-    </Card>
+            <Typography variant="h6">
+              Took {getFormatedTime(context.timeTaken) + " mins"}
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{ mx: 1 }}
+              size="small"
+              onClick={submitScore}
+            >
+              Submit
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ mx: 1 }}
+              size="small"
+              onClick={restart}
+            >
+              Re-try
+            </Button>
+            <Alert
+              severity="success"
+              variant="string"
+              sx={{
+                width: "60%",
+                m: "auto",
+                visibility: showAlert ? "visible" : "hidden",
+              }}
+            >
+              Score Updated.
+            </Alert>
+          </CardContent>
+        </Box>
+        <CardMedia component="img" sx={{ width: 220 }} image="./result.png" />
+      </Card>
+      <Answer qnAnswers={qnAnswers} />
+    </>
   );
 }
