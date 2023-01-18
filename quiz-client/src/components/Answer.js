eb50505eaec76927499601669afc256f36154833
AP <aleksandrpolishchuk@gmail.com>
2023-01-18 21:33:58 +0700

    add opportunity open 1 accordion at a time in Answer.js

--- quiz-client/src/components/Answer.js
+++ quiz-client/src/components/Answer.js
@@ -12,10 +12,21 @@ import React from "react";
 import { BASE_URL } from "../api";
 
 export default function Answer({ qnAnswers }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
     <Box sx={{ mt: 5, width: "100%", maxWidth: 640, mx: "auto" }}>
       {qnAnswers.map((item, j) => (
-        <Accordion disableGutters key={j}>
+        <Accordion
+          disableGutters
+          key={j}
+          expanded={expanded === j}
+          onChange={handleChange(j)}
+        >
           <AccordionSummary>
             <Typography sx={{ width: "90%", flexShrink: 0 }}>
               {item.qnInWords}
